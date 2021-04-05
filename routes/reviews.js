5b2e30525d559529190e9d1c053ea2af3a047fa1
Angel <gutierrezramosjoseangel@gmail.com>
2021-04-05 01:26:09 -0500

    Update and Delete Reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,7 +2,9 @@ const express = require('express');
 const {
     getReviews,
     getReview,
-    addReview
+    addReview,
+    updateReview,
+    deleteReview
 } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
@@ -20,6 +22,8 @@ router.route('/')
     .post(protect, authorize('user', 'admin'), addReview);
 
 router.route('/:id')
-    .get(getReview);
+    .get(getReview)
+    .put(protect, authorize('user', 'admin'), updateReview)
+    .delete(protect, authorize('user', 'admin'), deleteReview);
 
 module.exports = router;
